45a3d5896390d6ba3939e33bda18e94e09f2fd4d
Asmita Pathare <asmitapat9@gmail.com>
2020-05-25 23:50:15 +0530

    Modal Included

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// Modal-Section
+// Loading Modal
+$(function() {
+    $("#myModal").modal();
+});
+
+
+$(document).ready(function(){
+	$("#contactForm").submit(function(event){
+     alert("Thank You for responding. We will get in touch with you as soon as possible!!");
+	});
+});
